37d16d503069e71e38d3ac45e191af2ddc200f26
inseonyun <yuninseon@naver.com>
2022-05-17 14:02:11 +0900

    python 파일 및 requirements.txt 수정

--- main.py
+++ main.py
@@ -5,27 +5,26 @@ book_title = []
 book_isbn = []
 book_writer = []
 
-data = {
-    'title':book_title,
-    'isbn':book_isbn,
-    'writer':book_writer
-}
+data = [
+    book_title,
+    book_isbn,
+    book_writer
+]
 
 def page(index):
-    html = requests.get('https://book.naver.com/bookdb/book_detail.naver?bid=')
+    return requests.get('https://book.naver.com/bookdb/book_detail.naver?bid=' + str(index))
 
-def crawler(): 
+def crawler(i): 
     #url = 'https://book.naver.com/bookdb/book_detail.naver?bid=15052904'
     #url = 'https://book.naver.com/bookdb/book_detail.naver?bid=21480555'
-    url = 'https://book.naver.com/bookdb/book_detail.naver?bid=2234942'
-    response = requests.get(url)
+    response = page(i)
     soup = BeautifulSoup(response.content, "html.parser")
 
     ### 책 이름 정보 가져옴
     h2_tag = soup.findAll('h2')
     title = ''
     for tag in h2_tag:
-        title = str(tag.text)
+        title = str(tag.text).replace('\xa0', ' ')
 
     ### 책 저자, ISBN 정보 가져옴
     writer = ''
@@ -35,7 +34,8 @@ def crawler():
         if '|' in tag.text:
             if '저자' in tag.text:
                 writer = tag.text.split('|')[0]
-                writer = writer.split(' ')[1]
+                writer = writer.split('저자 ')[1]
+                writer = writer.replace('\xa0', ' ')
 
             if 'ISBN' in tag.text:
                 isbn = tag.text.split('|')[1]
@@ -47,9 +47,12 @@ def crawler():
     book_isbn.append(isbn)
     book_writer.append(writer)
 
-    ### Check data
-    for row in data:
-        for d in data[row]:
-            print(d)
+for i in range(1, 10):
+    crawler(i)
 
-crawler()
+### Check data
+for row in range(len(data[0])):
+    d = []
+    for col in range(len(data)):
+        d.append(data[col][row])
+    print(d)
